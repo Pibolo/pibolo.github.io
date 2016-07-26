dbd66d4d02d24a148cd1377a8b9f7e8c4db5a905
Pibolo <jeremy.alluin@gmail.com>
2016-07-26 18:19:39 +0200

    init less et compil

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Document</title>
+	<link rel="stylesheet" type="text/css" href="styles/styles.min.css">
 </head>
 <body>
 	<h1>Salut le monde trop cool !</h1>
