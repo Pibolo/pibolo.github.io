40619a81726c704249edd3c389e83edcb0101d89
Pibolo <jeremy.alluin@gmail.com>
2016-07-29 15:51:25 +0200

    mise en place des dossiers et fichier du onepage

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,20 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
+var less = require('gulp-less');
 var browserSync = require('browser-sync');
 var connect = require('gulp-connect-php');
 
 
-gulp.task('sass', function () {
-  return gulp.src('assets/sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
+gulp.task('less', function () {
+  return gulp.src('assets/less/**/*.less')
+    .pipe(less().on('error', less.logError))
     .pipe(gulp.dest('assets/css'))
     .pipe(browserSync.reload({
       stream: true
     }));
 });
 
-gulp.task('watch',['browserSync', 'sass'], function(){
-  gulp.watch('./assets/sass/**/*.scss', ['sass']);
+gulp.task('watch',['browserSync', 'less'], function(){
+  gulp.watch('./assets/less/**/*.less', ['less']);
   gulp.watch('*.php').on('change', browserSync.reload);
 });
 
@@ -31,3 +31,5 @@ gulp.task('browserSync', function(){
     })
   })
 });
+
+gulp.task('default', ['less']);
