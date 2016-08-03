d9ef816af03734d10ec4f0b6fb91fa52ed5e11c1
Nicolas Rotier <nrotier@qosenergy.com>
2016-08-03 16:17:20 +0200

    test 1

--- send.php
+++ send.php
@@ -1,14 +1,37 @@
-<?php
-	require 'vendor/autoload.php';
-	use Mailgun\Mailgun;
-						# Instantiate the client.
-	$mgClient = new Mailgun(getenv('MAILGUN_API_KEY'));
-	$domain = getenv('MAIL_DOMAIN');
-		# Make the call to the client.
-	$result = $mgClient->sendMessage($domain, array(
-		'from'    => $_POST["name"] . '<'. $_POST["email"] .'>',
-		'to'      => '<' . getenv('MAIL_TO') . '>',
-		'subject' => "Pibolo CV",
-		'text'    => $_POST["message"]
-		));
-?>
+<!DOCTYPE html>
+<html lang="en">
+	<body>
+		<nav class="nav-send">
+			<ul>
+				<li><a href="index.php#contact">Retour au site</a></li>
+			</ul>
+		</nav>
+
+		<div class="wrapper-send">
+			<div class="pres-send">
+
+				<?php
+					require 'vendor/autoload.php';
+					use Mailgun\Mailgun;
+
+					var_dump($_SERVER['MAIL_DOMAIN']);
+					var_dump($_ENV);
+					var_dump(getenv('MAIL_DOMAIN'));
+					# Instantiate the client.
+					$mgClient = new Mailgun(getenv('MAILGUN_API_KEY'));
+					$domain = getenv('MAIL_DOMAIN');
+					# Make the call to the client.
+					$result = $mgClient->sendMessage($domain, array(
+						'from'    => $_POST["name"] . '<'. $_POST["email"] .'>',
+						'to'      => '<' . getenv('MAIL_TO') . '>',
+						'subject' => "Pibolo CV",
+						'text'    => $_POST["message"]
+						));
+				?>
+				</div>
+		</div>
+
+
+	</body>
+</html>
+
