6d78cd72726b8bbe024ea27ad8127df4b13f1cfb
Pibolo <jeremy.alluin@gmail.com>
2016-08-03 15:07:42 +0200

    modif from

--- send.php
+++ send.php
@@ -1,3 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<link rel="stylesheet" type="text/css" href="style/style.css" />
+		<link rel="stylesheet" type="text/css" href="style/scrollpath.css" />
+		<link rel="stylesheet" href="popin/style/styles.css" />
+		<link rel="stylesheet" href="popin/style/iview.css" />
+		<link rel="stylesheet" href="popin/style/popin/style.css" />
+		<link rel="stylesheet" type="text/css" href="styles_form.css" />
+
+
+		<!--[if lt IE 9]>
+			<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+		<meta name="description" content="The plugin that lets you define custom scroll paths" />
+		<title>Jérémy Alluin Portfolio</title>
+	</head>
+	<body>
+		<nav class="nav-send">
+			<ul>
+				<li><a href="index.php">Retour au site</a></li>
+			</ul>
+		</nav>
+
+		<div class="wrapper-send">
+			<div class="pres-send">
 
 				<?php
 					require 'vendor/autoload.php';
@@ -14,3 +41,10 @@
 						'text'    => $_POST["message"]
 						));
 				?>
+				</div>
+		</div>
+
+
+	</body>
+</html>
+
